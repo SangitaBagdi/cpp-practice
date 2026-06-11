bacadc2628ec65ac008b6deef797d0678cd04e58
Sangita Bagdi <136609501+SangitaBagdi@users.noreply.github.com>
2026-06-11 17:22:45 +0530

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,s1=0,s2=1,ms;
+    cout<<"Enter the number you want fibonacci series upto:";
+    cin>>n;
+   // cout<<s1 <<" "<<s2 <<" ";
+    for(int i=3;i<=n;i++){
+         cout<<s1 <<" "<<s2 <<" ";
+        ms=s1+s2;
+        cout<<ms<<" ";
+        s1=s2;
+        s2=ms;
+
+    }
+
+    return 0;
+}
